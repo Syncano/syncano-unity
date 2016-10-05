732a24db4265f37b8dc5c792f9447ea1ed59ace7
Sebastian Sztangierski <sebastian.sztangierski@pixzell.pl>
2016-10-05 14:09:39 +0200

    [Problem/Solution] Add missing DataObjectPermissions enum

---
+++ Assets/Syncano/Enum/DataObjectPermissions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+
+public enum DataObjectPermissions {
+	
+	[JsonProperty("none")]
+	NONE,
+
+	[JsonProperty("read")]
+	READ,
+
+	[JsonProperty("write")]
+	WRITE,
+
+	[JsonProperty("full")]
+	FULL
+}
